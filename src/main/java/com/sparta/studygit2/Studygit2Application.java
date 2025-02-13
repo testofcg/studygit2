3d0eb7beed127f14bbe5dceb09492b85675a29ea
CGYOON <ckdrmsdl9999@naver.com>
2025-02-13 22:19:53 +0900

    develop br

--- src/main/java/com/sparta/studygit2/Studygit2Application.java
+++ src/main/java/com/sparta/studygit2/Studygit2Application.java
@@ -9,6 +9,7 @@ public class Studygit2Application {
 	public static void main(String[] args) {
 		int num=1;
 		int num=2;
+		int developmake=1;
 		SpringApplication.run(Studygit2Application.class, args);
 	}
 

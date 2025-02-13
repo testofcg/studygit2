3fae2dd1608ca57c36c4bc7bc19807a9ef32262f
CGYOON <ckdrmsdl9999@naver.com>
2025-02-13 22:21:05 +0900

    crush

--- src/main/java/com/sparta/studygit2/Studygit2Application.java
+++ src/main/java/com/sparta/studygit2/Studygit2Application.java
@@ -8,7 +8,7 @@ public class Studygit2Application {
 
 	public static void main(String[] args) {
 		int num=1;
-		int num=2;
+		int num=3;
 		int developmake=1;
 		SpringApplication.run(Studygit2Application.class, args);
 	}
